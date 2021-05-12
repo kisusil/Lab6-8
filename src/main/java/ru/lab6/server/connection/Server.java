544f3c9d1c0f96b5f4e3fc3759de52e87c495b66
Olga <312637@niuitmo.ru>
2021-05-13 00:14:11 +0300

    Прием подключений и чтение запросов

--- src/main/java/ru/lab6/server/connection/Server.java
+++ src/main/java/ru/lab6/server/connection/Server.java
@@ -5,23 +5,27 @@ import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+
 
 public class Server {
     private SocketChannel socketChannel;
 
-    public void receiveRequest() throws IOException, ClassNotFoundException {
+    public ByteBuffer receiveRequest() throws IOException, ClassNotFoundException {
         socketChannel = createSocketChannel();
-        ObjectInputStream objectInputStream = new ObjectInputStream(socketChannel.socket().getInputStream());
-        JsonObject request = (JsonObject) objectInputStream.readObject(); //получили сериализованный объект
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        socketChannel.read(buf);
+        buf.flip();
         socketChannel.close();
+        return buf;
     }
 
     private SocketChannel createSocketChannel() throws IOException {
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open(); //вызов серверного сокета
-        serverSocketChannel.socket().bind(new InetSocketAddress(9100)); //слушаем какой-нибутб локальный адрес
-        socketChannel = serverSocketChannel.accept(); //принимаем подключение
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(8000));
+        socketChannel = serverSocketChannel.accept();
         return socketChannel;
     }
 }
