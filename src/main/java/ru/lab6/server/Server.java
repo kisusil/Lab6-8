20efc86f71253c00a8bd330d9c6aaa98b3297215
Olga <312637@niuitmo.ru>
2021-05-23 20:08:22 +0300

    Logging

--- src/main/java/ru/lab6/server/Server.java
+++ src/main/java/ru/lab6/server/Server.java
@@ -1,6 +1,7 @@
 package ru.lab6.server;
 
 import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
 import ru.lab6.common.request.Request;
 import ru.lab6.common.response.Response;
 
@@ -11,11 +12,14 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.logging.log4j.*;
+
 
 public class Server {
     private final ServerSocketChannel serverSocketChannel;
     private SocketChannel socketChannel;
     private final int port;
+    private static Logger logger = LogManager.getLogger(Server.class);
 
     public Server(int port){
         this.port = port;
@@ -33,9 +37,10 @@ public class Server {
             buf.get(bytes);
 
             String jsonString = new String(bytes, StandardCharsets.UTF_8);
-
+            logger.info("Request received.");
             return new Gson().fromJson(jsonString, Request.class);
         } catch (IOException e) {
+            logger.error("Error while receiving request.");
             throw new RuntimeException(e);
         }
     }
@@ -45,7 +50,9 @@ public class Server {
             byte[] bytes = response.json().getBytes(StandardCharsets.UTF_8);
             ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
             socketChannel.write(byteBuffer);
+            logger.info("Response sent.");
         } catch (IOException e){
+            logger.error("Error while response sending.");
             throw new RuntimeException(e);
         }
     }
@@ -54,8 +61,10 @@ public class Server {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.socket().bind(new InetSocketAddress(port));
+            logger.info("Server socket channel created.");
             return serverSocketChannel;
         } catch (IOException e) {
+            logger.fatal("Error while server socket channel created.");
             throw new RuntimeException(e);
         }
     }
@@ -63,7 +72,9 @@ public class Server {
     public void acceptNewClient() {
         try {
             socketChannel = serverSocketChannel.accept();
+            logger.info("Client connected.");
         } catch (IOException e) {
+            logger.error("Error while connecting client.");
             throw new RuntimeException(e);
         }
     }
@@ -71,7 +82,9 @@ public class Server {
     public void closeConnection() {
         try {
             socketChannel.close();
+            logger.info("Connection stopped.");
         } catch (IOException e) {
+            logger.error("Error while stopping connection.");
             throw new RuntimeException(e);
         }
     }
