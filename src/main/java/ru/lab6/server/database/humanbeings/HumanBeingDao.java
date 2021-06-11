97fe7f29bfbf31d5ddf0bc2623f3d15e18826869
Olga <312637@niuitmo.ru>
2021-06-11 14:18:11 +0300

    HumanBeingDao

---
+++ src/main/java/ru/lab6/server/database/humanbeings/HumanBeingDao.java
@@ -0,0 +1,11 @@
+package ru.lab6.server.database.humanbeings;
+
+import ru.lab6.common.humanbeing.HumanBeing;
+
+import java.util.List;
+
+public interface HumanBeingDao {
+    void save(HumanBeing humanBeing);
+    void update(HumanBeing humanBeing);
+    void saveAll(List<HumanBeing> collection);
+}
